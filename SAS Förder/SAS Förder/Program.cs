d11c0e1b454dd21e4b631fbe3a4398735f9efbb4
Samael Morningstar <saito_epvp@web.de>
2019-09-13 15:05:28 +0200

    Update Thema 3

--- SAS Förder/SAS Förder/Program.cs
+++ SAS Förder/SAS Förder/Program.cs
@@ -13,37 +13,43 @@ namespace SAS_Förder
 			int choose = Convert.ToInt32(Console.ReadLine());
 			if (choose == 1)
 			{
+				double Kathete1 = 0, Kathete2 = 0, Hypotenuse = 0, a = 0;
 				Console.WriteLine("Hypotenusenberechnung");
 				Console.WriteLine("");
 				Console.WriteLine("Gib die Länge der ersten Kathete an");
-				double Kathete1 = Convert.ToDouble(Console.ReadLine());
+				Kathete1 = Convert.ToDouble(Console.ReadLine());
 				Console.WriteLine("");
 				Console.WriteLine("Gib die Länge der zweiten Kathete an");
-				double Kathete2 = Convert.ToDouble(Console.ReadLine());
+				Kathete2 = Convert.ToDouble(Console.ReadLine());
 				Console.WriteLine("");
-				double a = Kathete1 * Kathete1 + Kathete2 * Kathete2;
-				double Hypotenuse = Math.Sqrt(a);
+				a = Kathete1 * Kathete1 + Kathete2 * Kathete2;
+				Hypotenuse = Math.Sqrt(a);
 				Console.WriteLine("Die Hypotenuse beträgt: {0}", Hypotenuse);
 			}
 			if (choose == 2)
 			{
+				double b = 0, m = 0, nullstelle = 0;
 				Console.WriteLine("Nullstellenberechnung");
 				Console.WriteLine("");
 				Console.WriteLine("Geben Sie den Wert für b ein");
-				double b = Convert.ToDouble(Console.ReadLine());
+				b = Convert.ToDouble(Console.ReadLine());
 				Console.WriteLine("");
 				Console.WriteLine("Geben Sie den Wert für m ein");
-				double m = Convert.ToDouble(Console.ReadLine());
+				m = Convert.ToDouble(Console.ReadLine());
 				Console.WriteLine("");
 				if (m != 0)
 				{
-					double nullstelle = (-b / m);
+					nullstelle = (-b / m);
 					Console.WriteLine("Das Ergebnis lautet {0}", nullstelle);
 				}
 				else
 				{
 					Console.WriteLine("Du kannst b nicht durch 0 teilen! Bitte wähle eine andere Zahl!");
 				}
+			}
+			else
+			{
+				Console.WriteLine("Es wurde eine fehlerhafte Eingabe gemacht."); //anti-bug
 			}
 		}
 	}
