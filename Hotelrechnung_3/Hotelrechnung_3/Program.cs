1d798586a29ef9341f04fe5376bb468d370161a7
Samael Morningstar <saito_epvp@web.de>
2019-09-18 12:53:15 +0200

    ttt

--- Hotelrechnung_3/Hotelrechnung_3/Program.cs
+++ Hotelrechnung_3/Hotelrechnung_3/Program.cs
@@ -17,23 +17,23 @@ namespace Hotelrechnung
 			Console.WriteLine("(1) Normales Zimmer");
 			Console.WriteLine("(2) Premium Zimmer");
 			Console.WriteLine("(3) Fist Class Zimmer");
-			double ZimmerArt = Convert.ToDouble(Console.ReadLine());
+			int ZimmerArt = Convert.ToInt32(Console.ReadLine());
 			Console.WriteLine("");
 			Console.WriteLine("Wie viele Personen wollen einchecken?");
-			double Personen = Convert.ToDouble(Console.ReadLine());
-			Console.WriteLine("");
-			Console.WriteLine("Wie viele Tage bleiben die Personen im Hotel?");
-			double Aufenthaltsdauer = Convert.ToDouble(Console.ReadLine());
-			double Standard_Room = 70, Premium_Room = 150, First_Class = 250;
+			int Personen = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("\nWie viele Tage bleiben die Personen im Hotel?");
+			int Aufenthaltsdauer = Convert.ToInt32(Console.ReadLine());
+			double Standard_Room = 70, Premium_Room = 150, First_Class = 250, Kind1 = 100, Kind2 = 70, Kind3 = 40;
 			double Nettopreis = 0, Gesamtpreis = 0;
 			if (ZimmerArt == 1)
 			{
-				string Zimmer = "Normal_Room";
-				Nettopreis = Standard_Room * Aufenthaltsdauer * Personen;
-				Gesamtpreis = Nettopreis + Nettopreis * MwSt / 100;
-				Console.WriteLine("");
-				Console.WriteLine("Der Gesamtpreis für ein Normales Zimmer beträgt {0}", Gesamtpreis);
-				Console.WriteLine("Bitte überprüfe folgende Eingaben: {0} Personen checken für {1} Tag(e) ein. Zimmertyp: {2}", Personen, Aufenthaltsdauer, Zimmer);
+
+					string Zimmer = "Normal_Room";
+					Nettopreis = Standard_Room * Aufenthaltsdauer * Personen;
+					Gesamtpreis = Nettopreis + Nettopreis * MwSt / 100;
+					Console.WriteLine("");
+					Console.WriteLine("Der Gesamtpreis für ein Normales Zimmer beträgt {0}", Gesamtpreis);
+					Console.WriteLine("Bitte überprüfe folgende Eingaben: {0} Personen checken für {1} Tag(e) ein. Zimmertyp: {2}", Personen, Aufenthaltsdauer, Zimmer);
 			}
 			if (ZimmerArt == 2)
 			{
