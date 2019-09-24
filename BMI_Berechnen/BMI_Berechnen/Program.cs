925229695994e00df5a01a245430cce598b9f88e
Samael Morningstar <saito_epvp@web.de>
2019-09-24 14:03:32 +0200

    update

---
+++ BMI_Berechnen/BMI_Berechnen/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BMI_Berechnen
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Bitte Gewicht in Kg eingeben (aufgerundet):");
+			string gewicht1 = Console.ReadLine();
+			Console.WriteLine("Bitte die Körpergröße in meter eingeben (Achtung! Eingabe NICHT mit einen punkt trennen, sondern ein komma verwenden.)");
+			string größe1 = Console.ReadLine();
+			Console.WriteLine("Ergebnis: ");
+			bmi_ausgabe(gewicht1, größe1);
+		}
+		public static void bmi_ausgabe(string gewicht, string größe)
+		{
+			double gewicht2 = Convert.ToDouble(gewicht);
+			double größe2 = Convert.ToDouble(größe);
+			double BMIWert = gewicht2 / (größe2 * größe2);
+			if (BMIWert < 18.5)
+			{
+				Console.WriteLine("Untergewicht");
+			}
+			else if (BMIWert >= 25)
+			{
+				Console.WriteLine("Übergewicht");
+			}
+			else if (BMIWert >= 18.5 && BMIWert < 25)
+			{
+				Console.WriteLine("Normalgewicht");
+			}
+		}
+	}
+}
--- Hotelrechnung_3/Hotelrechnung_3/Program.cs
+++ Hotelrechnung_3/Hotelrechnung_3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 /*
 	Dominik Brodeufel
 	BFT91	 
@@ -23,22 +24,110 @@ namespace Hotelrechnung
 			int Personen = Convert.ToInt32(Console.ReadLine());
 			Console.WriteLine("\nWie viele Tage bleiben die Personen im Hotel?");
 			int Aufenthaltsdauer = Convert.ToInt32(Console.ReadLine());
-			double Standard_Room = 70, Premium_Room = 150, First_Class = 250, Kind1 = 100, Kind2 = 70, Kind3 = 40;
+			double Standard_Room = 70, Premium_Room = 150, First_Class = 250;
 			double Nettopreis = 0, Gesamtpreis = 0;
+
+			//#########################################################################################
+			
+			Console.WriteLine(); //
+			Console.WriteLine("Wie viele von den Personen sind Kinder?");
+			int AnzahlKinder = Convert.ToInt32(Console.ReadLine());
+
+			if(AnzahlKinder > Personen)
+			{
+				Console.WriteLine("Fehler mehr Kinder als insgesamt Personen ?"); //Fehlerausgabe
+
+				return; 
+			}
+
+			int[] KindAlterListe = new int[AnzahlKinder]; 
+
+			for(int Kind = 0; Kind < AnzahlKinder; Kind++) 
+			{
+				Console.WriteLine();
+				Console.WriteLine("Wie alt ist Kind {0} ?", Kind + 1);
+				int Alter = Convert.ToInt32(Console.ReadLine()); 
+
+				KindAlterListe[Kind] = Alter; 
+			}
+
+			//#########################################################################################
+			
 			if (ZimmerArt == 1)
 			{
+				string Zimmer = "Normal_Room";
 
-					string Zimmer = "Normal_Room";
-					Nettopreis = Standard_Room * Aufenthaltsdauer * Personen;
-					Gesamtpreis = Nettopreis + Nettopreis * MwSt / 100;
-					Console.WriteLine("");
-					Console.WriteLine("Der Gesamtpreis für ein Normales Zimmer beträgt {0}", Gesamtpreis);
-					Console.WriteLine("Bitte überprüfe folgende Eingaben: {0} Personen checken für {1} Tag(e) ein. Zimmertyp: {2}", Personen, Aufenthaltsdauer, Zimmer);
+				//#########################################################################################
+
+				Nettopreis = Standard_Room * Aufenthaltsdauer * (Personen - AnzahlKinder);
+
+				for (int Kind = 0; Kind < AnzahlKinder; Kind++)
+				{
+					int KindAlter = KindAlterListe[Kind];
+					
+					if(KindAlter <= 6)
+					{
+						//Frei
+					}
+
+					else if(KindAlter <= 11) //Von 7 bis 11
+					{
+						Nettopreis += Standard_Room * Aufenthaltsdauer * 0.4; //0.4 = 40%
+					}
+
+					else if(KindAlter <= 14) //Von 12 bis 14
+					{
+						Nettopreis += Standard_Room * Aufenthaltsdauer * 0.7; //0.7 = 70%
+					}
+
+					else //Von 15 bis Infinity
+					{
+						Nettopreis += Standard_Room * Aufenthaltsdauer; //100%
+					}
+				}
+
+				//#########################################################################################
+
+				Gesamtpreis = Nettopreis + Nettopreis * MwSt / 100;
+				Console.WriteLine("");
+				Console.WriteLine("Der Gesamtpreis für ein Normales Zimmer beträgt {0}", Gesamtpreis);
+				Console.WriteLine("Bitte überprüfe folgende Eingaben: {0} Personen checken für {1} Tag(e) ein. Zimmertyp: {2}", Personen, Aufenthaltsdauer, Zimmer);
 			}
 			if (ZimmerArt == 2)
 			{
 				string Zimmer = "Premium_Room";
-				Nettopreis = Premium_Room * Aufenthaltsdauer * Personen;
+
+				//#########################################################################################
+
+				Nettopreis = Premium_Room * Aufenthaltsdauer * (Personen - AnzahlKinder); 
+
+				for (int Kind = 0; Kind < AnzahlKinder; Kind++)
+				{
+					int KindAlter = KindAlterListe[Kind];
+
+					if (KindAlter <= 6)
+					{
+						//Frei
+					}
+
+					else if (KindAlter <= 11) //Von 7 bis 11
+					{
+						Nettopreis += Premium_Room * Aufenthaltsdauer * 0.4; //0.4 = 40%
+					}
+
+					else if (KindAlter <= 14) //Von 12 bis 14
+					{
+						Nettopreis += Premium_Room * Aufenthaltsdauer * 0.7; //0.7 = 70%
+					}
+
+					else //Von 15 bis Infinity
+					{
+						Nettopreis += Premium_Room * Aufenthaltsdauer; //100%
+					}
+				}
+
+				//#########################################################################################
+
 				Gesamtpreis = Nettopreis + Nettopreis * MwSt / 100;
 				Console.WriteLine("");
 				Console.WriteLine("Der Gesamtpreis für ein Premium Zimmer beträgt {0} Euro", Gesamtpreis);
@@ -47,7 +136,38 @@ namespace Hotelrechnung
 			if (ZimmerArt == 3)
 			{
 				string Zimmer = "FirstClass_Room";
-				Nettopreis = First_Class * Aufenthaltsdauer * Personen;
+
+				//#########################################################################################
+
+				Nettopreis = First_Class * Aufenthaltsdauer * (Personen - AnzahlKinder);
+
+				for (int Kind = 0; Kind < AnzahlKinder; Kind++)
+				{
+					int KindAlter = KindAlterListe[Kind];
+
+					if (KindAlter <= 6)
+					{
+						//Frei
+					}
+
+					else if (KindAlter <= 11) //Von 7 bis 11
+					{
+						Nettopreis += First_Class * Aufenthaltsdauer * 0.4; //0.4 = 40%
+					}
+
+					else if (KindAlter <= 14) //Von 12 bis 14
+					{
+						Nettopreis += First_Class * Aufenthaltsdauer * 0.7; //0.7 = 70%
+					}
+
+					else //Von 15 bis Infinity
+					{
+						Nettopreis += First_Class * Aufenthaltsdauer; //100%
+					}
+				}
+
+				//#########################################################################################
+
 				Gesamtpreis = Nettopreis + Nettopreis * MwSt / 100;
 				Console.WriteLine("");
 				Console.WriteLine("Der Gesamtpreis für ein FirstClasss Zimmer beträgt {0} Euro.", Gesamtpreis);
